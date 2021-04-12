7cca66df199a2b08f87c620a2562576658f18002
Péter Kalicz <kaliczp@gmail.com>
2021-04-12 14:28:04 +0200

    Formula

--- lm.R
+++ lm.R
@@ -2,7 +2,12 @@
 x <- sample(1:100, 200, replace = TRUE)
 y <- -2 * x + rnorm(n = 200, mean = 1, sd = 100)
 
-plot(x,y)
+## adattábla létrehozás
+adat <- data.frame(a = x, b = y)
 
+## Ábrázolás adattáblával
+plot(b ~ a, data = adat)
+
+## Korreláció
 cov(x,y)
 cor(x,y)
