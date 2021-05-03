d3a15cc4af51992280b7f8ad6eff45c6622e0d8c
Péter Kalicz <kaliczp@gmail.com>
2021-05-03 13:51:30 +0200

    Trendfelület

---
+++ spatial2.R
@@ -0,0 +1,14 @@
+library(MASS)
+library(spatial)
+data(topo)
+topo
+class(topo)
+View(topo)
+plot(topo)
+plot(topo[,1:2])
+plot(topo[,c("x","y")], asp=T)
+text(topo$x, topo$y, topo$z, adj = c(0,0))
+
+topo.ls <- surf.ls(2, topo)
+trsurf <- trmat(topo.ls, 0, 6.5, 0, 6.5, 50)
+contour(trsurf, asp = TRUE)
