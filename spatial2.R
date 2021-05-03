16618d743cce486a453c0cbed318d1eefdf011dc
Péter Kalicz <kaliczp@gmail.com>
2021-05-03 14:02:53 +0200

    correlogram, variogram

--- spatial2.R
+++ spatial2.R
@@ -12,3 +12,6 @@ text(topo$x, topo$y, topo$z, adj = c(0,0))
 topo.ls <- surf.ls(2, topo)
 trsurf <- trmat(topo.ls, 0, 6.5, 0, 6.5, 50)
 contour(trsurf, asp = TRUE)
+
+correlogram(topo.ls, 30)
+variogram(topo.ls, 30)
